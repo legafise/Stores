efa3c10ff90664fb21bc090d6272502f2a717e9a
legafise <lashkevich1974@gmail.com>
2020-03-18 18:25:36 +0300

    Redact user entity

--- src/main/java/com/lashkevich/stores/entity/User.java
+++ src/main/java/com/lashkevich/stores/entity/User.java
@@ -1,6 +1,7 @@
 package com.lashkevich.stores.entity;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class User {
     private long id;
@@ -10,13 +11,14 @@ public class User {
     private String password;
     private String email;
     private LocalDate birthDate;
-    private long role;
-    private long city;
+    private Role role;
+    private City city;
+    private Basket basket;
 
     public User() {
     }
 
-    public User(long id, String name, String surname, String login, String password, String email, LocalDate birthDate, long role, long city) {
+    public User(long id, String name, String surname, String login, String password, String email, LocalDate birthDate, Role role, City city, Basket basket) {
         this.id = id;
         this.name = name;
         this.surname = surname;
@@ -26,6 +28,7 @@ public class User {
         this.birthDate = birthDate;
         this.role = role;
         this.city = city;
+        this.basket = basket;
     }
 
     public long getId() {
@@ -84,57 +87,50 @@ public class User {
         this.birthDate = birthDate;
     }
 
-    public long getRole() {
+    public Role getRole() {
         return role;
     }
 
-    public void setRole(long role) {
+    public void setRole(Role role) {
         this.role = role;
     }
 
-    public long getCity() {
+    public City getCity() {
         return city;
     }
 
-    public void setCity(long city) {
+    public void setCity(City city) {
         this.city = city;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
+    public void setBasket(Basket basket) {
+        this.basket = basket;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o == this){
-            return true;
-        }
-
-        if(o == null || getClass() != o.getClass()) {
-            return true;
-        }
-
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-
-        if (id == user.id && name.equals(user.name) && surname.equals(user.surname) && login.equals(user.login) &&
-                password.equals(user.password) && email.equals(user.email) && birthDate.equals(user.birthDate) &&
-                role == user.role && city == user.city) {
-            return true;
-        } else {
-            return false;
-        }
+        return id == user.id &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(surname, user.surname) &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(birthDate, user.birthDate) &&
+                Objects.equals(role, user.role) &&
+                Objects.equals(city, user.city) &&
+                Objects.equals(basket, user.basket);
     }
 
     @Override
     public int hashCode() {
-        int code = 32;
-        code += id;
-        code += name.hashCode();
-        code += surname.hashCode();
-        code += login.hashCode();
-        code += password.hashCode();
-        code += email.hashCode();
-        code += birthDate.hashCode();
-        code += role;
-        code += city;
-
-        return code;
+        return Objects.hash(id, name, surname, login, password, email, birthDate, role, city, basket);
     }
 
     @Override
@@ -146,9 +142,10 @@ public class User {
                 ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
-                ", birthDate='" + birthDate + '\'' +
+                ", birthDate=" + birthDate +
                 ", role=" + role +
                 ", city=" + city +
+                ", basket=" + basket +
                 '}';
     }
 }
