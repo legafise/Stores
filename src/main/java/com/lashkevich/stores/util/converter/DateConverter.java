210d1ab193a3d970cbba69eb082537cd151e3f02
legafise <lashkevich1974@gmail.com>
2020-03-18 18:22:47 +0300

    Redact date converter

--- src/main/java/com/lashkevich/stores/util/converter/DateConverter.java
+++ src/main/java/com/lashkevich/stores/util/converter/DateConverter.java
@@ -4,8 +4,8 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 public class DateConverter {
-    public static java.sql.Date convertLocalDateToDate(LocalDate localDate) {
-        return java.sql.Date.valueOf(localDate);
+    public static Date convertLocalDateToDate(LocalDate localDate) {
+        return Date.valueOf(localDate);
     }
 
     public static LocalDate convertDateToLocalDate(Date date) {
