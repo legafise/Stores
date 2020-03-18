ce60334fd7e8ee7d6b4ccccf0f375b658edcf9ed
legafise <lashkevich1974@gmail.com>
2020-03-18 18:32:43 +0300

    Redact dao mapper

--- src/main/java/com/lashkevich/stores/dao/mapper/DaoMapper.java
+++ src/main/java/com/lashkevich/stores/dao/mapper/DaoMapper.java
@@ -5,81 +5,103 @@ import com.lashkevich.stores.util.converter.DateConverter;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DaoMapper {
-    private static final String ID = "id";
-    private static final String NAME = "name";
     private static final String SURNAME = "surname";
     private static final String LOGIN = "login";
     private static final String PASSWORD = "password";
     private static final String EMAIL = "email";
     private static final String BIRTH_DATE = "birth_date";
     private static final String ROLE_ID = "role_id";
+    private static final String ROLE_NAME = "role_name";
     private static final String CITY_ID = "city_id";
+    private static final String CITY_NAME = "city_name";
     private static final String COUNTRY_ID = "country_id";
-    private static final String SUMMARY = "summary";
-    private static final String DESCRIPTION = "description";
+    private static final String COUNTRY_NAME = "country_name";
     private static final String GOOD_ID = "good_id";
+    private static final String GOOD_NAME = "good_name";
+    private static final String GOOD_SUMMARY = "good_summary";
+    private static final String GOOD_DESCRIPTION = "good_description";
     private static final String USER_ID = "user_id";
+    private static final String USER_NAME = "user_name";
     private static final String PRICE = "price";
+    private static final String QUANTITY = "quantity";
 
     public static Role mapRole(ResultSet resultSet) throws SQLException {
         Role role = new Role();
-        role.setId(resultSet.getLong(ID));
-        role.setName(resultSet.getString(NAME));
+        role.setId(resultSet.getLong(ROLE_ID));
+        role.setName(resultSet.getString(ROLE_NAME));
         return role;
     }
 
     public static Country mapCountry(ResultSet resultSet) throws SQLException {
         Country country = new Country();
-        country.setId(resultSet.getLong(ID));
-        country.setName(resultSet.getString(NAME));
+        country.setId(resultSet.getLong(COUNTRY_ID));
+        country.setName(resultSet.getString(COUNTRY_NAME));
         return country;
     }
 
     public static User mapUser(ResultSet resultSet) throws SQLException {
         User user = new User();
-        user.setId(resultSet.getLong(ID));
-        user.setName(resultSet.getString(NAME));
-        user.setSurname(resultSet.getString(SURNAME));
-        user.setLogin(resultSet.getString(LOGIN));
-        user.setPassword(resultSet.getString(PASSWORD));
-        user.setEmail(resultSet.getString(EMAIL));
-        user.setBirthDate(DateConverter.convertDateToLocalDate(resultSet.getDate(BIRTH_DATE)));
-        user.setRole(resultSet.getLong(ROLE_ID));
-        user.setCity(resultSet.getLong(CITY_ID));
+        Map<Good, Integer> goods = new HashMap<>();
+        user.setBasket(new Basket(goods));
+        while (resultSet.next()) {
+            if (user.getId() != resultSet.getLong(USER_ID) && user.getId() != 0) {
+                resultSet.previous();
+                return user;
+            }
+            if (user.getId() != resultSet.getLong(USER_ID)) {
+                fillUserData(resultSet, user);
+            }
+
+            goods.put(mapGood(resultSet), resultSet.getInt(QUANTITY));
+        }
         return user;
     }
 
     public static City mapCity(ResultSet resultSet) throws SQLException {
         City city = new City();
-        city.setId(resultSet.getLong(ID));
-        city.setName(resultSet.getString(NAME));
-        city.setCountry(resultSet.getLong(COUNTRY_ID));
+        city.setId(resultSet.getLong(CITY_ID));
+        city.setName(resultSet.getString(CITY_NAME));
+        city.setCountry(mapCountry(resultSet));
         return city;
     }
 
     public static Good mapGood(ResultSet resultSet) throws SQLException {
         Good good = new Good();
-        good.setId(resultSet.getLong(ID));
-        good.setName(resultSet.getString(NAME));
-        good.setSummary(resultSet.getString(SUMMARY));
-        good.setDescription(resultSet.getString(DESCRIPTION));
+        good.setId(resultSet.getLong(GOOD_ID));
+        good.setName(resultSet.getString(GOOD_NAME));
+        good.setSummary(resultSet.getString(GOOD_SUMMARY));
+        good.setDescription(resultSet.getString(GOOD_DESCRIPTION));
         return good;
     }
 
     public static Basket mapBasket(ResultSet resultSet) throws SQLException {
         Basket basket = new Basket();
-        basket.setGood(resultSet.getLong(GOOD_ID));
-        basket.setUser(resultSet.getLong(USER_ID));
+        Map<Good, Integer> goods = new HashMap<>();
+        goods.put(mapGood(resultSet), resultSet.getInt(QUANTITY));
         return basket;
     }
 
     public static GoodPrice mapGoodPrice(ResultSet resultSet) throws SQLException {
         GoodPrice goodPrice = new GoodPrice();
-        goodPrice.setGood(resultSet.getLong(GOOD_ID));
-        goodPrice.setCountry(resultSet.getLong(COUNTRY_ID));
-        goodPrice.setPrice(resultSet.getDouble(PRICE));
+        goodPrice.setCountry(mapCountry(resultSet));
+        goodPrice.setGood(mapGood(resultSet));
+        goodPrice.setPrice(resultSet.getBigDecimal(PRICE));
         return goodPrice;
+    }
+
+    private static void fillUserData(ResultSet resultSet, User user) throws SQLException {
+        user.setId(resultSet.getLong(USER_ID));
+        user.setName(resultSet.getString(USER_NAME));
+        user.setSurname(resultSet.getString(SURNAME));
+        user.setLogin(resultSet.getString(LOGIN));
+        user.setPassword(resultSet.getString(PASSWORD));
+        user.setEmail(resultSet.getString(EMAIL));
+        user.setBirthDate(DateConverter.convertDateToLocalDate(resultSet.getDate(BIRTH_DATE)));
+        user.setRole(mapRole(resultSet));
+        user.setCity(mapCity(resultSet));
     }
 }
