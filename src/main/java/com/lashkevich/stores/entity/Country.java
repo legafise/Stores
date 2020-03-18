6ef5ffb090e30753e6dcd152247fc672f114cfcf
legafise <lashkevich1974@gmail.com>
2020-03-18 18:27:52 +0300

    Redact country entity

--- src/main/java/com/lashkevich/stores/entity/Country.java
+++ src/main/java/com/lashkevich/stores/entity/Country.java
@@ -6,7 +6,7 @@ public class Country {
     private long id;
     private String name;
 
-    public Country(int id, String name) {
+    public Country(long id, String name) {
         this.id = id;
         this.name = name;
     }
