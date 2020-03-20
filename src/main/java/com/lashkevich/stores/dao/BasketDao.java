959edeea81daa19cfdbfc50f349f8383d8c90e50
legafise <lashkevich1974@gmail.com>
2020-03-20 18:25:21 +0300

    Redact basket dao logic

--- src/main/java/com/lashkevich/stores/dao/BasketDao.java
+++ src/main/java/com/lashkevich/stores/dao/BasketDao.java
@@ -6,13 +6,13 @@ import com.lashkevich.stores.exception.DaoStoreException;
 import java.util.List;
 
 public interface BasketDao {
-    void add(Basket basket) throws DaoStoreException;
+    void add(Basket basket, long userId) throws DaoStoreException;
 
-    List<Basket> getAll() throws DaoStoreException;
+    List<Basket> findAll() throws DaoStoreException;
 
-    Basket getByUser(long id) throws DaoStoreException;
+    Basket findByUser(long id) throws DaoStoreException;
 
-    void update(long good, long user, Basket basket) throws DaoStoreException;
+    void update(Basket basket, long userId) throws DaoStoreException;
 
-    void remove(long goodId, long userId) throws DaoStoreException;
+    void remove(long userId) throws DaoStoreException;
 }
--- src/main/java/com/lashkevich/stores/dao/impl/BasketDaoImpl.java
+++ src/main/java/com/lashkevich/stores/dao/impl/BasketDaoImpl.java
@@ -3,6 +3,7 @@ package com.lashkevich.stores.dao.impl;
 import com.lashkevich.stores.dao.BasketDao;
 import com.lashkevich.stores.dao.mapper.DaoMapper;
 import com.lashkevich.stores.entity.Basket;
+import com.lashkevich.stores.entity.Good;
 import com.lashkevich.stores.exception.ConnectionStoreException;
 import com.lashkevich.stores.exception.DaoStoreException;
 import com.lashkevich.stores.util.connection.ConnectionUtil;
@@ -10,28 +11,37 @@ import com.lashkevich.stores.util.connection.ConnectionUtil;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class BasketDaoImpl implements BasketDao {
-    private static final String ADD_TO_BASKET_SQL = "INSERT INTO baskets (good_id, user_id) VALUES (?, ?);";
-    private static final String FIND_ALL_USERS_AND_THEIR_GOODS_SQL = "SELECT good_id, user_id FROM baskets;";
-    private static final String FIND_GOODS_BY_USER_SQL = "SELECT good_id, user_id FROM baskets WHERE user_id = ?;";
-    private static final String UPDATE_BASKET_SQL = "UPDATE baskets SET good_id = ?, user_id = ? WHERE good_id = ? AND user_id = ?;";
-    private static final String DELETE_USER_AND_THEY_GOODS_BY_THEIR_ID_SQL = "DELETE FROM baskets WHERE good_id = ? AND user_id = ?;";
+    private static final String ADD_TO_BASKET_SQL = "INSERT INTO baskets (good_id, user_id, quantity) VALUES (?, ?, ?);";
+    private static final String FIND_ALL_USERS_AND_THEIR_GOODS_SQL = "SELECT baskets.user_id AS primary_key_good_id, baskets.user_id," +
+            " baskets.quantity, goods.id AS good_id, goods.name AS good_name, goods.summary, goods.description FROM baskets LEFT JOIN goods ON baskets.good_id = goods.id;";
+    private static final String FIND_GOODS_BY_USER_SQL = "SELECT baskets.user_id AS primary_key_good_id, baskets.user_id," +
+            " baskets.quantity, goods.id AS good_id, goods.name AS good_name, goods.summary, goods.description FROM baskets LEFT JOIN goods ON baskets.good_id = goods.id WHERE user_id = ?;";
+    private static final String DELETE_BASKET_SQL = "DELETE FROM baskets WHERE user_id = ?;";
 
     @Override
-    public void add(Basket basket) throws DaoStoreException {
-        try(Connection connection = ConnectionUtil.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(ADD_TO_BASKET_SQL)) {
-            preparedStatement.setLong(1, basket.getGood());
-            preparedStatement.setLong(2, basket.getUser());
-            preparedStatement.executeUpdate();
-        } catch (ConnectionStoreException | SQLException e) {
+    public void add(Basket basket, long userId) throws DaoStoreException {
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(ADD_TO_BASKET_SQL)) {
+            
+            for (Map.Entry<Good, Integer> goods : basket.getGoods().entrySet()) {
+                preparedStatement.setLong(1, goods.getKey().getId());
+                preparedStatement.setLong(2, userId);
+                preparedStatement.setLong(3, goods.getValue());
+                preparedStatement.executeUpdate();
+            }
+            
+        } catch (ConnectionStoreException |
+                SQLException e) {
             throw new DaoStoreException(e);
         }
+
     }
 
     @Override
-    public List<Basket> getAll() throws DaoStoreException {
+    public List<Basket> findAll() throws DaoStoreException {
         try (Connection connection = ConnectionUtil.getConnection();
              Statement statement = connection.createStatement()) {
             List<Basket> baskets = new ArrayList<>();
@@ -48,7 +58,7 @@ public class BasketDaoImpl implements BasketDao {
     }
 
     @Override
-    public Basket getByUser(long id) throws DaoStoreException {
+    public Basket findByUser(long id) throws DaoStoreException {
         try (Connection connection = ConnectionUtil.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(FIND_GOODS_BY_USER_SQL)) {
             preparedStatement.setLong(1, id);
@@ -66,25 +76,30 @@ public class BasketDaoImpl implements BasketDao {
     }
 
     @Override
-    public void update(long goodId, long userId, Basket basket) throws DaoStoreException {
+    public void update(Basket basket, long userId) throws DaoStoreException {
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_BASKET_SQL)) {
-            preparedStatement.setLong(1, basket.getGood());
-            preparedStatement.setLong(2, basket.getUser());
-            preparedStatement.setLong(3, goodId);
-            preparedStatement.setLong(4, userId);
-            preparedStatement.executeUpdate();
+             PreparedStatement deletePreparedStatement = connection.prepareStatement(DELETE_BASKET_SQL);
+             PreparedStatement addPreparedStatement = connection.prepareStatement(ADD_TO_BASKET_SQL)) {
+            deletePreparedStatement.setLong(1, userId);
+            deletePreparedStatement.executeUpdate();
+
+            for (Map.Entry<Good, Integer> goods : basket.getGoods().entrySet()) {
+                addPreparedStatement.setLong(1, goods.getKey().getId());
+                addPreparedStatement.setLong(2, userId);
+                addPreparedStatement.setLong(3, goods.getValue());
+                addPreparedStatement.executeUpdate();
+            }
+
         } catch (SQLException | ConnectionStoreException e) {
             throw new DaoStoreException(e);
         }
     }
 
     @Override
-    public void remove(long goodId, long userId) throws DaoStoreException {
+    public void remove(long userId) throws DaoStoreException {
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USER_AND_THEY_GOODS_BY_THEIR_ID_SQL)) {
-            preparedStatement.setLong(1, goodId);
-            preparedStatement.setLong(2, userId);
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_BASKET_SQL)) {
+            preparedStatement.setLong(1, userId);
             preparedStatement.executeUpdate();
         } catch (SQLException | ConnectionStoreException e) {
             throw new DaoStoreException(e);
