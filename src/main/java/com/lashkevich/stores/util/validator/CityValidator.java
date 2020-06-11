27a6212302acc06389e7d5c2163d431aba2ca938
legafise <lashkevich1974@gmail.com>
2020-06-11 12:34:12 +0300

    Redact city validator logic

--- src/main/java/com/lashkevich/stores/util/validator/CityValidator.java
+++ src/main/java/com/lashkevich/stores/util/validator/CityValidator.java
@@ -1,18 +1,13 @@
 package com.lashkevich.stores.util.validator;
 
 import com.lashkevich.stores.entity.City;
-import com.lashkevich.stores.entity.Country;
 
 public class CityValidator {
     public static boolean validate(City city) {
-        return city != null && validateName(city.getName()) && validateCountry(city.getCountry());
+        return city != null && validateName(city.getName()) && CountryValidator.validate(city.getCountry());
     }
 
     private static boolean validateName(String name) {
         return name != null && name.length() >= 1 && name.length() <= 45;
-    }
-
-    private static boolean validateCountry(Country country) {
-        return CountryValidator.validate(country);
     }
 }
