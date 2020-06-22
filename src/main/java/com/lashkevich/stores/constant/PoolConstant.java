1acf780b7a1fd7b7eb72883384afdeb0cc353670
legafise <lashkevich1974@gmail.com>
2020-06-22 20:51:03 +0300

    Create pool constants

---
+++ src/main/java/com/lashkevich/stores/constant/PoolConstant.java
@@ -0,0 +1,9 @@
+package com.lashkevich.stores.constant;
+
+public class PoolConstant {
+    public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/nonamestore";
+    public static final String TEST_URL = "jdbc:mysql://localhost:3306/test_nonamestore";
+    public static final String USER = "root";
+    public static final String PASSWORD = "1234";
+}
